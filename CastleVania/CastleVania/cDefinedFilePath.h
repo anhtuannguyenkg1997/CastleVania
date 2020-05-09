4d6ff10089bad8721411b949ccb2d830bf98779c
anhtuankg1997 <anhtuankg1997@gmail.com>
2020-05-09 15:44:03 +0700

    a

--- CastleVania/CastleVania/cDefinedFilePath.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-#define GENERAL_OBJECT_FILEPATH  L"Objects\\General_Objects\\General_Objects.txt"
-
-#define HUD_FILEPATH L"Objects\\General_Objects\\HUD.txt"
-
-
-#define MAIN_FILEPATH L"castleVania.txt"
--- CastleVania/CastleVania/definedEnemy.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-//ENEMY
-// Enemy (chung cho Zombie, Black Leopard, Bat...)
-#define ACTIVE			0
-#define DESTROYED		1
-#define INACTIVE		2
